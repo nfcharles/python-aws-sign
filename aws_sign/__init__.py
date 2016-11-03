a03d27504f435be0c1985c39340caf1ac75e2a00
Navil Charles <navil.charles@gmail.com>
2016-11-03 17:38:57 -0400

    Added comments to ServiceConstants

--- aws_sign/__init__.py
+++ aws_sign/__init__.py
@@ -2,9 +2,29 @@ import copy
 import re
 
 class ServiceConstants(object):
+    """Logical grouping of AWS service constants
+
+    Each AWS service has a list of values representative of its configuration;
+    this class serves as a base set of these parameters.
+
+    Sublclasses should override `URL_REGEX` for `from_url` inherited behavior to
+    work properly.  See URL_REGEX comments below for more information.
+
+    Example:
+        class DynamoDBServiceConstants(ServiceConstants):
+            URL_REGEX = re.compile(....)
+            
+            def __init__(self, *args, dynamo_foo=None, dynamo_bar=None):
+                super(DynamoDBServiceConstants, self).__init__(*args):
+                self.dynamo_foo = dynamo_foo
+                self.dynamo_bar = dynamo_bar
+    """
     # Minimum required headers for signing requests
     HEADERS = {'host': None, 'x-amz-date': None}
 
+    # Parsed by 'from_url' method.  Matched group array is passed as *args list to
+    # constructor so ordinal positions of match values must match constructor args
+    # respectively.
     URL_REGEX = re.compile(r"""(?:https://)?    # scheme
                                (([\w\-]+)       # service
                                \.
@@ -20,6 +40,13 @@ class ServiceConstants(object):
         self.headers   = headers if headers else self._merge({'host': host})
     
     def _merge(self, override):
+        """Merges headers
+        
+        Parameters:
+            override: dict overrides
+            
+        Returns merged header dict
+        """
         merged = copy.copy(self.HEADERS)
         merged.update(override)
         return merged
@@ -30,6 +57,15 @@ class ServiceConstants(object):
 
     @classmethod
     def from_url(cls, url, pattern=None, **kwargs):
+        """Constructs ServiceConstants instance
+
+        Parameters:
+            url: url endpoint
+            pattern: regex override pattern
+            kwargs: passthru kwargs
+
+        Returns class instance
+        """
         regex = cls.URL_REGEX
         if pattern:
             regex = re.compile(pattern)
