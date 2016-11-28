2d4115e5a3b019f3e346a2c8b19e4b5b9c0904aa
Navil Charles <navil.charles@gmail.com>
2016-11-28 12:32:00 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,15 @@ setup(
     author_email = 'navil.charles@gmail.com',
     description = 'AWS Signing Tools',
     url = 'git@github.com:nfcharles/python-aws-sign.git',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: API Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        ],
     test_suite = 'nose.collector',
     packages = [
         'aws_sign',
